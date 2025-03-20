80fe48f389e9b3873ba1425ba59fcf5dd4c8e993
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2025-03-20 11:09:10 +0900

    [level 2] Title: 기능개발, Time: 6.58 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+    
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        Queue<Integer> queue = new LinkedList<Integer>();
+        
+        for(int i=0; i<progresses.length; i++){
+            queue.add((int)Math.ceil((100.0-progresses[i])/speeds[i]));        
+        }
+        
+        while(!queue.isEmpty()){
+		int minDays = queue.poll();
+            int count = 1;
+
+            while(!queue.isEmpty() && queue.peek() <= minDays){
+                queue.poll();
+                count++;
+            }
+
+            answer.add(count);
+        }
+
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
