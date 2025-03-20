cec443499dcdb5ca8010f9d28d7e405fc22f42cf
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2025-03-20 10:17:47 +0900

    [level 1] Title: 폰켓몬, Time: 3.93 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
+        }
+        
+        answer = Math.min(set.size(), nums.length / 2);
+        
+        return answer;
+    }
+}
