f5b2b023813efd6df34da1cace0a804fc806ac5b
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2025-06-20 10:27:53 +0900

    [Silver IV] Title: 수열 (Easy), Time: 396 ms, Memory: 66588 KB -BaekjoonHub

---
+++ 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        long[] prefix = new long[arr.length+1];
+        for(int i = 1; i <= N; i++){
+            prefix[i] = prefix[i - 1] + arr[i - 1];
+        }
+
+        long answer = 0;
+        for(int i = 1; i <= N; i++){
+            answer += arr[i - 1] * (prefix[N] - prefix[i]);
+            answer %= 1000000007;
+        }
+
+        System.out.println(answer);
+    }
+}
