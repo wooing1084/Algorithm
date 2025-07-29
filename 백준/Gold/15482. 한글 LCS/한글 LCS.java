7957a5af95ab0ef8ef16b9270b41dbb2e9c7655e
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2025-07-29 12:56:00 +0900

    [Gold V] Title: 한글 LCS, Time: 164 ms, Memory: 20468 KB -BaekjoonHub

---
+++ 백준/Gold/15482. 한글 LCS/한글 LCS.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+
+        for(int i = 1; i <= str1.length(); i++){
+            for(int j = 1; j <= str2.length(); j++){
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+
+        System.out.println(dp[str1.length()][str2.length()]);
+    }
+}
