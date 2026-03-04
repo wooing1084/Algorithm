369c83364c0977588687621f633339c1425e0de5
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2026-03-05 00:49:39 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 124 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -15,47 +15,43 @@ class Main {
         }
 
         int[] last = new int[n];
+        int[] prev = new int[n];
+        int[] pos = new int[n];
         last[0] = arr[0];
         int lastIdx = 1;
-        int[] pos = new int[n];
-        int[] prev = new int[n];
         for(int i = 1; i < n; i++){
-            if(last[lastIdx - 1] < arr[i]){
-                pos[lastIdx] = i;
-                prev[i] = pos[lastIdx - 1];
-                last[lastIdx++] = arr[i];
+            int l = 0;
+            int r = lastIdx;
+            while(l < r){
+                int mid = (l + r) / 2;
+                if(last[mid] >= arr[i]) r = mid;
+                else l = mid + 1;
             }
-                
-            else {
-                int l = 0;
-                int r = lastIdx;
-                while(l < r){
-                    int mid = (l + r) / 2;
-                    if(arr[i] > last[mid])
-                        l = mid + 1;
-                    else
-                        r = mid;
-                }
-                last[l] = arr[i];
-                pos[l] = i;
-                if(l > 0){
-                    prev[i] = pos[l - 1];
-                }
-                 
+
+            pos[l] = i;
+            last[l] = arr[i];
+            if(l > 0)
+                prev[i] = pos[l - 1];
+            if (l == lastIdx){
+                lastIdx++;
             }
-            
         }
 
         int[] ans = new int[lastIdx];
-        int cur = pos[lastIdx - 1];
+        int now = pos[lastIdx - 1];
         for(int i = lastIdx - 1; i >= 0; i--){
-            ans[i] = arr[cur];
-            cur = prev[cur];
+            ans[i] = arr[now];
+            now = prev[now];
         }
+        
         StringBuilder sb = new StringBuilder();
-        sb.append(lastIdx + "\n");
-        for(int i = 0; i < lastIdx; i++)
-            sb.append(ans[i] + " ");
+        sb.append(lastIdx+ "\n");
+        for(int i = 0; i < lastIdx; i++){
+            sb.append(ans[i]+" ");
+        }
+
         System.out.println(sb.toString());
+
+        
     }
 }
